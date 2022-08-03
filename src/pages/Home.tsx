73eed6c39ad93ffbb18a35ec2b3cc0745ade44b7
Adrian Trujillo <adrian.td96@gmail.com>
2022-08-03 00:57:09 -0500

    added home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+interface HomeProps {
+
+}
+
+const Home: React.FC<HomeProps> = ({  }) => {
+    return (
+      <div className='home-two'>
+        <Footer />
+      </div>
+    );
+}
+
+export default Home;
